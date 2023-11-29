fcf52cffabfea52e462d4a9b9c83ecbc2154c4cb
LampCat <vikinguraron@gmail.com>
2023-11-29 18:54:34 +0000

    added list

--- Toodler/App.js
+++ Toodler/App.js
@@ -1,10 +1,11 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 import 'react-native-gesture-handler';
 import AppContainer from './src/routes';
 
 export default function App() {
+
   return (
     <AppContainer/>
   );
--- Toodler/src/views/List/index.js
+++ Toodler/src/views/List/index.js
@@ -1,11 +1,14 @@
 import React from "react";
-import { View, Text } from "react-native";
+import { FlatList, SafeAreaView, Text, View } from 'react-native'
 import styles from './styles'
+import { useRoute } from "@react-navigation/native"
 
-const List = () =>(
-    <View style={styles.container}>
-        <Text>Lists!</Text>
-    </View>
+export const filterDatabyId = (id, data) =>{
+    return data.filter((data) => data.id == id);
+};
+const List = ({navigation: { navigate }}) =>(
+    console.log("this.props.route.params.id")
+
 );
 
 export default List
