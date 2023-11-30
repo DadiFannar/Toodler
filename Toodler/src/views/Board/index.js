f6b9f860232c6bc5bafad119eb93a26ba337ea2f
LampCat <vikinguraron@gmail.com>
2023-11-30 12:04:38 +0000

    Fixed BoardView

--- Toodler/src/views/Board/index.js
+++ Toodler/src/views/Board/index.js
@@ -1,5 +1,5 @@
-import React from "react";
-import { View, Text } from "react-native";
+import React, {useState} from "react";
+import { View, Text} from "react-native";
 import styles from './styles'
 import BoardList from "../../components/BoardList";
 import data from '../../resources/data.json'
@@ -7,18 +7,22 @@ import { AntDesign } from '@expo/vector-icons';
 
 
 
-const Board = () =>(
-    <View>
+const Board = () =>{
+    const [currentData, setCurrentData] = useState(data);
+
+    return (
+        <View>
         <AntDesign name="pluscircle" size={80} style={styles.add}/>
         {/* <Text style= {styles.text}>Board</Text> */}
-        <BoardList data={data.boards.map(function(item){
+        <BoardList data={currentData.boards.map(function(item){
             return{
                 id:item.id,
                 name:item.name,
                 photo:item.thumbnailPhoto
             }
         })} />
-    </View> 
-);
+        </View> 
+    )
+};
 
 export default Board
