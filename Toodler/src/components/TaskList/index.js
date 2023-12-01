fa32442cc82ca73d83d76eea1c82524e8645bbbf
LampCat <vikinguraron@gmail.com>
2023-12-01 11:25:47 +0000

    Added TaskList component

---
+++ Toodler/src/components/TaskList/index.js
@@ -0,0 +1,56 @@
+import React from "react";
+import { FlatList, SafeAreaView, Text, TouchableHighlight, View } from 'react-native'
+import styles from './styles'
+import ImageThumbnail from '../ImageThumbnail'
+import {AntDesign} from '@expo/vector-icons'
+import { Ionicons } from '@expo/vector-icons';
+import { TouchableOpacity } from "react-native-gesture-handler";
+import { useNavigation } from '@react-navigation/native';
+
+const getTitleStyle = (isFinished) =>{
+  if(isFinished){
+    return styles.titleDone;
+  }
+  else{
+    return styles.title;
+  }
+}
+const getDescriptionStyle = (isFinished) =>{
+  if(isFinished){
+    return styles.descriptionDone;
+  }
+  else{
+    return styles.description;
+  }
+}
+const Item = ({navigation: { navigate }, item, boards, lists, tasks}) => (
+  <TouchableOpacity onPress={() => console.log("marking Item")}>
+    <View style={[styles.item]}>
+      <Ionicons name="close-circle-sharp" size={32} style={styles.close} />
+      <Text style={getTitleStyle(item.isFinished)}>{item.name}</Text>
+      <Text style={getDescriptionStyle(item.isFinished)}>{item.description}</Text>
+    </View>
+  </TouchableOpacity>
+);
+
+const TaskList = ({boards, lists, tasks, displayData}) => {
+    const navigation = useNavigation();
+    return (
+      <SafeAreaView>
+        <FlatList
+          data={displayData.map(function(item){
+            return{
+                id:item.id,
+                name:item.name,
+                description:item.description,
+                isFinished:item.isFinished
+            }
+            })}
+          renderItem={({item}) => <Item navigation={navigation} item={item} boards={boards} lists={lists} tasks={tasks}/>}
+          keyExtractor={item => item.id}
+        />
+      </SafeAreaView>
+    )
+}
+
+export default TaskList
--- Toodler/src/views/Task/index.js
+++ Toodler/src/views/Task/index.js
@@ -1,25 +1,13 @@
 import React from "react";
 import { FlatList, SafeAreaView, Text, View } from 'react-native'
 import styles from './styles'
-import { useRoute } from "@react-navigation/native"
+import TaskList from "../../components/TaskList";
 
 const Task = ({ route }) =>{
     const {boards, lists, tasks, displayData, listId} = route.params;
     return (
-        //TEST CODE REMOVE THIS AND ADD COMPONENT TASKLIST
         <View style={styles.container}>
-          <FlatList
-          data={displayData.map(function(item){
-            return{
-                id:item.id,
-                name:item.name,
-                description:item.description,
-                isFinished:item.isFinished
-            }
-            })}
-            keyExtractor={item => item.id}
-            renderItem={({item}) => <Text>{item.name}:{item.description}</Text>}
-        />
+            <TaskList boards={boards} lists={lists} displayData={displayData}/>
         </View>
     );
 };
